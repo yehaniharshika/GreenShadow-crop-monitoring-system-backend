813d564d796b2235f3f58aac389fe2bbfb030d72
yehaniharshika <pamunuwayehaniharshika@gmail.com>
2024-11-10 17:37:08 +0530

    implement UserStatus interface to UserDTO

--- src/main/java/lk/ijse/greenshadowcropmonitoringsystembackend/dto/impl/UserDTO.java
+++ src/main/java/lk/ijse/greenshadowcropmonitoringsystembackend/dto/impl/UserDTO.java
@@ -1,9 +1,15 @@
 package lk.ijse.greenshadowcropmonitoringsystembackend.dto.impl;
 
+import lk.ijse.greenshadowcropmonitoringsystembackend.dto.UserStatus;
 import lk.ijse.greenshadowcropmonitoringsystembackend.entity.Role;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-public class UserDTO {
-
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class UserDTO implements UserStatus {
     private String userId;
     private String name;
     private String email;
