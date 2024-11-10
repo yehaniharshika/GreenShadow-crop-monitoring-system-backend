2c6098ce05ad3116e630c01edb075cce5a1cbc37
yehaniharshika <pamunuwayehaniharshika@gmail.com>
2024-11-10 20:17:28 +0530

    implement JWTService methods

--- src/main/java/lk/ijse/greenshadowcropmonitoringsystembackend/service/JWTService.java
+++ src/main/java/lk/ijse/greenshadowcropmonitoringsystembackend/service/JWTService.java
@@ -1,4 +1,10 @@
 package lk.ijse.greenshadowcropmonitoringsystembackend.service;
 
+import org.springframework.security.core.userdetails.UserDetails;
+
 public interface JWTService {
+    String extractUserName(String token);
+    String generateToken(UserDetails user);
+    boolean validateToken(String token,UserDetails userDetails);
+    String refreshToken(UserDetails userDetails);
 }
