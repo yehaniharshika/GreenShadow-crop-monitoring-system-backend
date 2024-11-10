c5d626e48451f816c35ddaa6f16b6db7e3cd7bcb
yehaniharshika <pamunuwayehaniharshika@gmail.com>
2024-11-10 17:14:35 +0530

    create Role enum

---
+++ src/main/java/lk/ijse/greenshadowcropmonitoringsystembackend/dto/UserStatus.java
@@ -0,0 +1,4 @@
+package lk.ijse.greenshadowcropmonitoringsystembackend.dto;
+
+public interface UserStatus {
+}
--- src/main/java/lk/ijse/greenshadowcropmonitoringsystembackend/entity/Role.java
+++ src/main/java/lk/ijse/greenshadowcropmonitoringsystembackend/entity/Role.java
@@ -1,5 +1,8 @@
 package lk.ijse.greenshadowcropmonitoringsystembackend.entity;
 
 public enum Role {
-    MANAGER,ADMIN,USER
+    MANAGER,
+    SCIENTIST,
+    ADMINISTRATOR,
+    OTHER
 }
