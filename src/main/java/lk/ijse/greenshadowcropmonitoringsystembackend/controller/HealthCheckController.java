cd74b8e8c1aa36781952d90424afc501f3303196
yehaniharshika <pamunuwayehaniharshika@gmail.com>
2024-11-10 21:29:21 +0530

    create HealthCheckController to test

---
+++ src/main/java/lk/ijse/greenshadowcropmonitoringsystembackend/controller/HealthCheckController.java
@@ -0,0 +1,15 @@
+package lk.ijse.greenshadowcropmonitoringsystembackend.controller;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/health")
+public class HealthCheckController {
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public  String healthTest(){
+        return "Green-Shadow is working";
+    }
+}
