454545628b9ef5e266a15cd5dbe31ea325298a58
yehaniharshika <pamunuwayehaniharshika@gmail.com>
2024-11-11 09:28:20 +0530

    implement FieldNotFoundException custom exception

---
+++ src/main/java/lk/ijse/greenshadowcropmonitoringsystembackend/controller/FieldController.java
@@ -0,0 +1,4 @@
+package lk.ijse.greenshadowcropmonitoringsystembackend.controller;
+
+public class FieldController {
+}
---
+++ src/main/java/lk/ijse/greenshadowcropmonitoringsystembackend/exception/FieldNotFoundException.java
@@ -0,0 +1,9 @@
+package lk.ijse.greenshadowcropmonitoringsystembackend.exception;
+
+public class FieldNotFoundException extends RuntimeException{
+    public FieldNotFoundException(){}
+
+    public FieldNotFoundException(String message){}
+
+    public FieldNotFoundException(String message,Throwable cause){}
+}
