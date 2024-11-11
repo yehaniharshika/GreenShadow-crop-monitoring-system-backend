7e0859cbfa12a83e90c8e15a53f6d83e7cd0d490
yehaniharshika <pamunuwayehaniharshika@gmail.com>
2024-11-11 11:34:08 +0530

    implement FieldService interface

--- src/main/java/lk/ijse/greenshadowcropmonitoringsystembackend/service/FieldService.java
+++ src/main/java/lk/ijse/greenshadowcropmonitoringsystembackend/service/FieldService.java
@@ -1,4 +1,14 @@
 package lk.ijse.greenshadowcropmonitoringsystembackend.service;
 
+import lk.ijse.greenshadowcropmonitoringsystembackend.dto.FieldStatus;
+import lk.ijse.greenshadowcropmonitoringsystembackend.dto.impl.FieldDTO;
+
+import java.util.List;
+
 public interface FieldService {
+    void saveField(FieldDTO fieldDTO);
+    List<FieldDTO> getAllFields();
+    FieldStatus getField(String fieldCode);
+    void updateField(String fieldCode,FieldDTO fieldDTO);
+    void deleteField(String fieldCode);
 }
