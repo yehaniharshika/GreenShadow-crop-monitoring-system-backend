bd23c40ead007772005a9e3302ac577c48207ba0
yehaniharshika <pamunuwayehaniharshika@gmail.com>
2024-11-14 09:23:48 +0530

    implement StaffNotFoundException custom exception

---
+++ src/main/java/lk/ijse/greenshadowcropmonitoringsystembackend/exception/StaffNotFoundException.java
@@ -0,0 +1,9 @@
+package lk.ijse.greenshadowcropmonitoringsystembackend.exception;
+
+public class StaffNotFoundException extends RuntimeException{
+    public StaffNotFoundException(){}
+
+    public StaffNotFoundException(String message){}
+
+    public StaffNotFoundException(String message,Throwable cause){}
+}
