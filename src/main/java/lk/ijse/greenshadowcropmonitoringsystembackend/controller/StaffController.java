fbec0c72edf0c4b3d653e4eed2c2d465233057cd
yehaniharshika <pamunuwayehaniharshika@gmail.com>
2024-11-14 12:08:28 +0530

    implement update staff in StaffController

--- src/main/java/lk/ijse/greenshadowcropmonitoringsystembackend/controller/StaffController.java
+++ src/main/java/lk/ijse/greenshadowcropmonitoringsystembackend/controller/StaffController.java
@@ -3,12 +3,15 @@ package lk.ijse.greenshadowcropmonitoringsystembackend.controller;
 import lk.ijse.greenshadowcropmonitoringsystembackend.dto.StaffStatus;
 import lk.ijse.greenshadowcropmonitoringsystembackend.dto.impl.StaffDTO;
 import lk.ijse.greenshadowcropmonitoringsystembackend.exception.DataPersistException;
+import lk.ijse.greenshadowcropmonitoringsystembackend.exception.StaffNotFoundException;
 import lk.ijse.greenshadowcropmonitoringsystembackend.service.StaffService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/staff")
@@ -33,6 +36,28 @@ public class StaffController {
     @GetMapping(value = "/{staffId}",produces = MediaType.APPLICATION_JSON_VALUE)
     public StaffStatus getSelectedStaff(@PathVariable("staffId") String staffId){
         return staffService.getStaff(staffId);
+    }
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<StaffDTO> getAllStaff(){
+        return staffService.getAllStaff();
+    }
+
+    @PutMapping(value = "/{staffId}",consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<String> updateField(@PathVariable("staffId") String staffId,@RequestBody StaffDTO updateStaffDTO){
+        try {
+            if (updateStaffDTO == null){
+                return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+            }
+            staffService.updateStaff(staffId,updateStaffDTO);
+            return new ResponseEntity<>("Staff update successfully",HttpStatus.OK);
+        }catch (StaffNotFoundException e){
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }catch (Exception e){
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
 }
