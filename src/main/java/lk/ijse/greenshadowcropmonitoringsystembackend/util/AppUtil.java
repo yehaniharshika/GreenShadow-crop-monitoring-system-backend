bb09dfd9413a28c2143821ab09071dab1dd4933a
yehaniharshika <pamunuwayehaniharshika@gmail.com>
2024-11-16 16:52:15 +0530

    implement LogImage convert to base64 format

--- src/main/java/lk/ijse/greenshadowcropmonitoringsystembackend/util/AppUtil.java
+++ src/main/java/lk/ijse/greenshadowcropmonitoringsystembackend/util/AppUtil.java
@@ -9,7 +9,13 @@ public class AppUtil {
     }
 
     public static String cropImageToBase64(byte[] cropImage){
+        //cropImage convert to base64 format
         return Base64.getEncoder().encodeToString(cropImage);
+    }
+
+    public static String LogObservedImageToBase64(byte[] observedImage){
+        //cropImage convert to base64 format
+        return Base64.getEncoder().encodeToString(observedImage);
     }
 
 }
