2e3ac614a7df33f8acf10bdfdc485ce6e3a3f22a
yehaniharshika <pamunuwayehaniharshika@gmail.com>
2024-11-17 09:59:42 +0530

    implement update vehicle,delete vehicle in VehicleController

--- src/main/java/lk/ijse/greenshadowcropmonitoringsystembackend/controller/VehicleController.java
+++ src/main/java/lk/ijse/greenshadowcropmonitoringsystembackend/controller/VehicleController.java
@@ -2,7 +2,9 @@ package lk.ijse.greenshadowcropmonitoringsystembackend.controller;
 
 import lk.ijse.greenshadowcropmonitoringsystembackend.dto.VehicleStatus;
 import lk.ijse.greenshadowcropmonitoringsystembackend.dto.impl.VehicleDTO;
+import lk.ijse.greenshadowcropmonitoringsystembackend.entity.impl.VehicleEntity;
 import lk.ijse.greenshadowcropmonitoringsystembackend.exception.DataPersistException;
+import lk.ijse.greenshadowcropmonitoringsystembackend.exception.VehicleNotFoundException;
 import lk.ijse.greenshadowcropmonitoringsystembackend.service.VehicleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -42,5 +44,36 @@ public class VehicleController {
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public List<VehicleDTO> getAllVehicles(){
         return vehicleService.getAllVehicles();
+    }
+
+    @PutMapping(value = "/{vehicleCode}",consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<String> updateVehicle(@PathVariable("vehicleCode") String vehicleCode, @RequestBody VehicleDTO updateVehicleDTO){
+        try {
+            if (updateVehicleDTO == null){
+                return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+            }
+            vehicleService.updateVehicle(vehicleCode,updateVehicleDTO);
+            return new ResponseEntity<>("vehicle update successfully",HttpStatus.OK);
+        }catch (VehicleNotFoundException e){
+            e.printStackTrace();
+            return new ResponseEntity<>("vehicle not update",HttpStatus.BAD_REQUEST);
+        }catch (Exception e){
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @DeleteMapping(value = "/{vehicleCode}",produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<String> deleteVehicle(@PathVariable("vehicleCode") String vehicleCode){
+        try {
+            vehicleService.deleteVehicle(vehicleCode);
+            return new ResponseEntity<>("vehicle deleted successfully",HttpStatus.OK);
+        }catch (VehicleNotFoundException e){
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }catch (Exception e){
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
