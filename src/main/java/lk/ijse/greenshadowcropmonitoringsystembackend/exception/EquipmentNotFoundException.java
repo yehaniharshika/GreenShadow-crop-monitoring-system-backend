7724379ac85003a21e613d13a347953b37c94bd1
yehaniharshika <pamunuwayehaniharshika@gmail.com>
2024-11-17 10:10:22 +0530

    implement EquipmentNotFoundException custom exception

---
+++ src/main/java/lk/ijse/greenshadowcropmonitoringsystembackend/exception/EquipmentNotFoundException.java
@@ -0,0 +1,9 @@
+package lk.ijse.greenshadowcropmonitoringsystembackend.exception;
+
+public class EquipmentNotFoundException extends RuntimeException{
+    public EquipmentNotFoundException(){}
+
+    public EquipmentNotFoundException(String message){}
+
+    public EquipmentNotFoundException(String message,Throwable cause){}
+}
