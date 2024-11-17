99005d7435de05ec71026a17cf3445cc861e609b
yehaniharshika <pamunuwayehaniharshika@gmail.com>
2024-11-17 19:14:36 +0530

    implement UserDetails related methods in UserEntity

--- src/main/java/lk/ijse/greenshadowcropmonitoringsystembackend/dao/UserDAO.java
+++ src/main/java/lk/ijse/greenshadowcropmonitoringsystembackend/dao/UserDAO.java
@@ -4,6 +4,9 @@ import lk.ijse.greenshadowcropmonitoringsystembackend.entity.impl.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserDAO extends JpaRepository<UserEntity,String> {
+    Optional<UserEntity> findByEmail(String email);
 }
--- src/main/java/lk/ijse/greenshadowcropmonitoringsystembackend/entity/impl/UserEntity.java
+++ src/main/java/lk/ijse/greenshadowcropmonitoringsystembackend/entity/impl/UserEntity.java
@@ -7,13 +7,20 @@ import lk.ijse.greenshadowcropmonitoringsystembackend.entity.SuperEntity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
 @Entity
 @Table(name = "user")
-public class UserEntity implements SuperEntity {
+public class UserEntity implements SuperEntity, UserDetails {
     @Id
     private String email;
 
@@ -27,4 +34,36 @@ public class UserEntity implements SuperEntity {
     @OneToOne
     @JoinColumn(name = "staff_id")
     private StaffEntity staff;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Set<GrantedAuthority> authorities = new HashSet<>();
+        authorities.add(new SimpleGrantedAuthority("ROLE_"+role.name()));
+        return authorities;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
 }
