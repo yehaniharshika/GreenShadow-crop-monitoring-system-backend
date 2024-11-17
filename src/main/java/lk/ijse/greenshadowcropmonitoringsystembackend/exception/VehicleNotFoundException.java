5c415ae533009734854e868b6078ac0bb9fcb0bc
yehaniharshika <pamunuwayehaniharshika@gmail.com>
2024-11-17 07:46:47 +0530

    implement VehicleNotFoundException custom exception

---
+++ src/main/java/lk/ijse/greenshadowcropmonitoringsystembackend/exception/VehicleNotFoundException.java
@@ -0,0 +1,9 @@
+package lk.ijse.greenshadowcropmonitoringsystembackend.exception;
+
+public class VehicleNotFoundException extends RuntimeException{
+    public VehicleNotFoundException(){}
+
+    public VehicleNotFoundException(String message){}
+
+    public VehicleNotFoundException(String message,Throwable cause){}
+}
