52bc0e8b3b2854ef97d9e15ff11c825af96b9f7c
yehaniharshika <pamunuwayehaniharshika@gmail.com>
2024-11-17 09:35:56 +0530

    implement get vehicle,get all vehicles in VehicleController

--- src/main/java/lk/ijse/greenshadowcropmonitoringsystembackend/controller/VehicleController.java
+++ src/main/java/lk/ijse/greenshadowcropmonitoringsystembackend/controller/VehicleController.java
@@ -1,5 +1,6 @@
 package lk.ijse.greenshadowcropmonitoringsystembackend.controller;
 
+import lk.ijse.greenshadowcropmonitoringsystembackend.dto.VehicleStatus;
 import lk.ijse.greenshadowcropmonitoringsystembackend.dto.impl.VehicleDTO;
 import lk.ijse.greenshadowcropmonitoringsystembackend.exception.DataPersistException;
 import lk.ijse.greenshadowcropmonitoringsystembackend.service.VehicleService;
@@ -7,10 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/vehicles")
@@ -32,5 +32,15 @@ public class VehicleController {
             e.printStackTrace();
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
+    }
+
+    @GetMapping(value = "/{vehicleCode}",produces = MediaType.APPLICATION_JSON_VALUE)
+    public VehicleStatus getSelectedVehicle(@PathVariable("vehicleCode") String vehicleCode){
+        return vehicleService.getVehicle(vehicleCode);
+    }
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<VehicleDTO> getAllVehicles(){
+        return vehicleService.getAllVehicles();
     }
 }
